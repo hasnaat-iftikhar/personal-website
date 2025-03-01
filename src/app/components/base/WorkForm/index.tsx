d3daf44964b2e74342400f54c52c4a7a55f1a7ea
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2025-03-02 04:18:36 +0500

    worked on bug fixings

--- src/app/components/base/WorkForm/index.tsx
+++ src/app/components/base/WorkForm/index.tsx
@@ -23,7 +23,7 @@ const WorkForm = () => {
     const [isModalActive, setIsModalActive] = useState<boolean>(false);
 
     const getTitle = useMemo(() => {
-        switch(currentStep) {
+        switch (currentStep) {
             case 1:
                 return "What do you need?";
             case 2:
@@ -41,7 +41,7 @@ const WorkForm = () => {
         setIsModalActive(modal.isActive);
     }, [modal.isActive]);
 
-    if(!isModalActive) {
+    if (!isModalActive) {
         return null;
     };
 
@@ -49,7 +49,7 @@ const WorkForm = () => {
         <Modal
             onClose={() => handleModalActiveState(false)}
         >
-            <div className='max-w-[92%] md:max-w-[600px] w-full flex flex-col justify-center items-center gap-[42px]'>
+            <div className='max-w-[92%] md:max-w-[600px] w-full flex flex-col items-center gap-[24px] md:gap-[42px] md:h-auto h-[500px] md:overflow-visible overflow-y-scroll md:justify-center justify-start mt-auto md:mt-0 mb-[36px] md:mb-0 hidden-vertical-scrollBar'>
                 <div className='flex flex-col gap-2 md:gap-3 text-center'>
                     <p className='text-[#DD7E00] text-[20px] md:text-[24px]'>0{currentStep}/05</p>
                     <h3 className={`${MediaSans.className} text-[40px] md:text-[48px] leading-[100%] text-[#171717]`}>
--- src/app/components/ui/Modal/index.tsx
+++ src/app/components/ui/Modal/index.tsx
@@ -2,12 +2,17 @@ import React, { FC } from 'react';
 
 // Type definations
 import { ModalProps } from './index.d';
-import { X } from 'lucide-react';
+import { ArrowLeft, X } from 'lucide-react';
+import { useWorkForm } from '../../base/WorkForm/context/context';
 
 const Modal: FC<ModalProps> = ({
     children,
     onClose = () => {}
 }) => {
+    const {
+        handlePreviousStep
+    } = useWorkForm();
+
     return (
         <div
             onClick={onClose}
@@ -17,12 +22,21 @@ const Modal: FC<ModalProps> = ({
                 onClick={(e) => e.stopPropagation()}
                 className='relative max-w-[1280px] h-[94dvh] md:h-[90dvh] lg:h-[720px] bg-white border-2 border-solid border-[#E8E8E8] rounded-[28px] flex justify-center items-center w-[90%] 2xl:w-full'
             >
-                <button
-                    onClick={onClose}
-                    className='w-[42px] h-[42px] rounded-[12px] bg-[#E8E8E8] flex justify-center items-center absolute top-[34px] right-[34px]'
-                >
-                    <X width={24} height={24} className='text-[#1B1B1B]' />
-                </button>
+                <div className='absolute flex justify-between items-center top-[22px] md:top-[34px] left-[22px] md:left-[34px] right-[22px] md:right-[34px] w-[-webkit-fill-available] h-[42px]'>
+                    <button
+                        onClick={handlePreviousStep}
+                        className='mr-auto w-[42px] h-[42px] rounded-[12px] bg-[#E8E8E8] flex justify-center items-center'
+                    >
+                        <ArrowLeft width={24} height={24} className='text-[#1B1B1B]' />
+                    </button>
+                    
+                    <button
+                        onClick={onClose}
+                        className='ml-auto w-[42px] h-[42px] rounded-[12px] bg-[#E8E8E8] flex justify-center items-center'
+                    >
+                        <X width={24} height={24} className='text-[#1B1B1B]' />
+                    </button>
+                </div>
 
                 {children}
             </div>
