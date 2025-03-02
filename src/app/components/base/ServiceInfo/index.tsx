cce56ef2083806273cbe0165ceb31f95a183c658
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2025-03-03 02:54:45 +0500

    fixed deployment bugs

--- src/app/components/base/ServiceInfo/index.tsx
+++ src/app/components/base/ServiceInfo/index.tsx
@@ -15,7 +15,7 @@ const ServiceInfo:FC<ServiceInfoProps> = ({
   reverse = false
 }) => {
   return (
-    <div className={`flex justify-between items-center flex-col ${reverse ? "2xl:flex-row-reverse" : "2xl:flex-row"} gap-6 2xl:gap-0`}>
+    <div className={`flex justify-between items-center flex-col ${reverse ? "xl:flex-row-reverse" : "xl:flex-row"} gap-6 2xl:gap-0`}>
       <div className='flex-1 max-w-[530px] flex flex-col gap-4'>
         <div className='flex flex-col gap-2'>
           <p className='text-[16px] md:text-[20px] text-[#F68D00] tracking-[32%]'>{headline}</p>
--- src/app/components/ui/Accordion/index.d.ts
+++ src/app/components/ui/Accordion/index.d.ts
@@ -1,5 +1,3 @@
-import { ReactNode } from "react";
-
 export interface AccordionProps {
     title: string;
     content: string;
