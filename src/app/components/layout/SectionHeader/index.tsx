13e9ebe2c3bbf2b4226494252bbccdee1a46b702
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2025-03-22 01:31:55 +0500

    Worked on some ui fixings in mobile

--- src/app/components/layout/SectionHeader/index.tsx
+++ src/app/components/layout/SectionHeader/index.tsx
@@ -16,12 +16,12 @@ const SectionHeader: FC<SectionHeaderProps> = ({
     return (
         <div className='w-full flex flex-col gap-[42px] md:gap-[64px]'>
             <div className='flex flex-col justify-center items-center gap-2'>
-                <h2 className={`${MediaSans.className} text-[54px] md:text-[64px] xl:text-[72px] leading-[100%] text-center tracking-[1px] ${titleClassName}`}>
+                <h2 className={`${MediaSans.className} text-[48px] md:text-[64px] xl:text-[72px] leading-[100%] text-center tracking-[1px] ${titleClassName}`}>
                     {title}
                 </h2>
 
                 {description && (
-                    <p className={`text-[18px] md:text-[20px] leading-[140%] text-center ${descriptionClassName}`}>{description}</p>
+                    <p className={`max-w-[310px] md:max-w-[auto] mx-auto text-[18px] md:text-[20px] leading-[140%] text-center ${descriptionClassName}`}>{description}</p>
                 )}
             </div>
         
--- src/app/components/ui/Accordion/index.tsx
+++ src/app/components/ui/Accordion/index.tsx
@@ -25,7 +25,7 @@ const Accordion: FC<AccordionProps> = ({
         className="w-full text-left py-5 md:py-7 flex justify-between items-start md:items-center gap-2 transition-all duration-300"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <span className={`${MediaSans.className} text-[36px] md:text-[40px] 2xl:text-[44px] leading-[100%] flex-1 tracking-[0.5px]`}>{title}</span>
+        <span className={`${MediaSans.className} text-[32px] md:text-[40px] 2xl:text-[44px] leading-[100%] flex-1 tracking-[0.5px]`}>{title}</span>
         <div className={`mt-2 md:mt-0 w-[36px] h-[36px] md:w-[42px] md:h-[42px] rounded-full bg-black flex justify-center items-center transform transition-transform duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`}>
           <ArrowDown width={24} height={24} className="text-white w-[18px] h-[18px] md:w-[24px] md:h-[24px]" />
         </div>
