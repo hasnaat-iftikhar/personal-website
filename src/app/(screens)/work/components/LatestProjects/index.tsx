844addff3f4f14dba9d498ae37ca05f70ec5ab26
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2025-03-26 17:45:46 +0500

    Worked on projects section

--- src/app/(screens)/work/components/LatestProjects/index.tsx
+++ src/app/(screens)/work/components/LatestProjects/index.tsx
@@ -5,33 +5,35 @@ import { Container, SectionHeader } from '@/app/components';
 
 // Fonts
 import Card from '@/app/components/ui/Card';
+import { projectsData } from '@/app/mock';
 
 const LatestProjects = () => {
-  return (
-    <section className='bg-[#1B1B1B] py-[96px] md:py-[120px]'>
-        <Container>
-            <SectionHeader title='Latest Projects' titleClassName='text-white'>
-                <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 md:gap-4'>
-                    <Card
-                        title='Project name'
-                        description='Project description here...'
-                        image='/'
-                    />
-                    <Card
-                        title='Project name'
-                        description='Project description here...'
-                        image='/'
-                    />
-                    <Card
-                        title='Project name'
-                        description='Project description here...'
-                        image='/'
-                    />
-                </div>
-            </SectionHeader>            
-        </Container>
-    </section>
-  )
+    const projects = projectsData.map(project => ({
+        id: project.id,
+        title: project.title,
+        thumbnail: project.media.thumbnail,
+        purpose: project.purpose
+    })).slice(0, 3);
+
+    return (
+        <section className='bg-[#1B1B1B] py-[96px] md:py-[120px]'>
+            <Container>
+                <SectionHeader title='Latest Projects' titleClassName='text-white'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 md:gap-4'>
+                        {projects.map((p) => (
+                            <Card
+                                key={p.id}
+                                id={p.id}
+                                title={p.title}
+                                description={p.purpose}
+                                image={p.thumbnail}
+                            />
+                        ))}
+                    </div>
+                </SectionHeader>
+            </Container>
+        </section>
+    )
 }
 
 export default LatestProjects;
--- src/app/(screens)/work/components/ProjectsCarousel/index.tsx
+++ src/app/(screens)/work/components/ProjectsCarousel/index.tsx
@@ -8,6 +8,7 @@ import "swiper/css/pagination"
 import "swiper/css/navigation"
 
 import { Autoplay } from "swiper/modules"
+import Image from "next/image"
 
 const ProjectsCarousel = () => {
   return (
@@ -32,10 +33,25 @@ const ProjectsCarousel = () => {
           transform: "translateX(0)",
         }}
       >
-        {[1, 2, 3, 4, 1, 2, 3, 4].map((slideNumber) => (
+        {[
+          '/portfolio/mini-thumbnails/fweygo.svg',
+          '/portfolio/mini-thumbnails/kove.svg',
+          '/portfolio/mini-thumbnails/cambie.svg',
+          '/portfolio/mini-thumbnails/fweygo.svg',
+          '/portfolio/mini-thumbnails/kove.svg',
+          '/portfolio/mini-thumbnails/cambie.svg',
+          '/portfolio/mini-thumbnails/fweygo.svg',
+          '/portfolio/mini-thumbnails/kove.svg',
+          '/portfolio/mini-thumbnails/cambie.svg'
+        ].map((slideNumber) => (
           <SwiperSlide key={slideNumber} className="!w-auto transition-transform">
-            <div className="w-[360px] md:w-[520px] aspect-video bg-[#F1F1F1] rounded-[18px] md:rounded-[28px] flex justify-center items-center">
-              Slide {slideNumber}
+            <div className="relative w-[360px] md:w-[520px] aspect-video bg-[#F1F1F1] rounded-[18px] overflow-hidden md:rounded-[16px]">
+              <Image
+                src={slideNumber}
+                alt="Project"
+                fill
+                className="object-cover"
+              />
             </div>
           </SwiperSlide>
         ))}
